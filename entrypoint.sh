babc60fb721241d1e80ef06107987ea9bc4ced96
toby1991 <t.post@hotmail.com>
2017-09-08 15:22:55 +0800

    add laravel vendor to builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
+# build laravel installer
 composer require -vvv "laravel/installer"
-/vendor/laravel/installer/laravel new laravel
 tar -czvf laravel_installer.tar.gz /vendor
 mv laravel_installer.tar.gz /builds/
+
+# build laravel
+/vendor/laravel/installer/laravel new laravel
+cd /laravel
+composer install -vvv
+cd /
 tar -czvf laravel_release.tar.gz /laravel
 mv laravel_release.tar.gz /builds/ 
